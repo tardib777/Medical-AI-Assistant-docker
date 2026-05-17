82bbaf22b8a785032ee1a6c84b55c4a2cea38ca6
Tareq Diab <tardib777@gmail.com>
2026-05-18 00:21:35 +0300

    Add files via upload

---
+++ app/Enums/BloodTypeEnum.php
@@ -0,0 +1,15 @@
+<?php
+ 
+ namespace App\Enums;
+
+ Enum BloodTypeEnum: string
+ {
+    case A_plus='A+';
+    case A_minus='A-';
+    case B_plus='B+';
+    case B_minus='B-';
+    case AB_plus='AB+';
+    case AB_minus='AB-';
+    case O_plus='O+';
+    case O_minus='O-';
+ }
---
+++ app/Enums/GenderEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum GenderEnum: string
+{
+    case MALE = 'male';
+    case FEMALE = 'female';
+    case MALE_AR = 'ذكر';
+    case FEMALE_AR = 'أنثى';
+}
