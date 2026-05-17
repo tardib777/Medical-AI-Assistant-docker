305d2fe5a8591d266840fc9b67545f53bb119c15
Tareq Diab <tardib777@gmail.com>
2026-05-18 01:14:46 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::put('/profile/update',[ProfileController::class,'update']);
+    Route::get('/profile/show',[ProfileController::class,'show']);
+    Route::post('/chat/send', [ChatController::class, 'chat'])->middleware('medical.session');
 });
+
